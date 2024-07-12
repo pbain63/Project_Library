8e448fe17ddc6a79fdbbb36d2fd053ed43aa8e11
pbain63 <prodipbain63@gmail.com>
2024-07-12 22:20:11 +0600

    Add event listener to show-dialog button in script.js

--- script.js
+++ script.js
@@ -2,10 +2,7 @@ const showDialogBtn = document.getElementById("show-dialog");
 const dialog = document.getElementById("dialog");
 const dialogClose = document.getElementById("dialog-close");
 
-// showDialogBtn.addEventListener("click", () => {
-//   dialog.showModal();
-// });
+showDialogBtn.addEventListener("click", () => {
+  dialog.showModal();
+});
 
-// dialogClose.addEventListener("click", () => {
-//   dialog.close();
-// });

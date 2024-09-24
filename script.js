8161c3f29b95d1ee9376fbb55814c821e1ca7fdb
pbain63 <prodipbain63@gmail.com>
2024-09-24 21:52:22 +0600

    Add JS code to script.js

--- script.js
+++ script.js
@@ -8,6 +8,7 @@ showDialogBtn.addEventListener("click", () => {
 
 closeDialog.addEventListener("click", () => {
   dialog.close();
+  dialogForm.reset();
 });
 
 const myLibrary = [];
@@ -17,135 +18,106 @@ function Book(title, author, pages, readingStatus) {
   this.author = author;
   this.pages = pages;
   this.readingStatus = readingStatus;
-  this.info = function () {
-    return `${this.title} ${this.author} ${this.pages} ${this.readingStatus}`;
-  };
 }
-let newBook = new Book("Hobbit", "p", 9, "Read");
-// console.log(newBook.info());
-
-let book1 = newBook.info();
-myLibrary.push(book1);
-
-// console.log(myLibrary[0]);
 
 const titleInput = document.getElementById("title");
 const authorInput = document.getElementById("author");
 const numberOfPagesInput = document.getElementById("number-pages");
 const readingOption = document.getElementById("reading-option");
 const addLibButton = document.getElementById("submit");
+const dialogForm = document.getElementById("dialog-form");
 // const addLibButton = document.querySelector("#submit");
-const dialogForm = document.querySelector(".dialog-form");
+// const dialogForm = document.querySelector(".dialog-form");
 
 const bookContainer = document.querySelector(".book-cards-container");
-const showNewBookCard = document.querySelector(".new-book-items-card");
-const showNewBookTitle = document.getElementById("new-book-items-show-title");
-const showNewBookAuthor = document.getElementById("new-book-items-show-author");
-const showNewBookPages = document.getElementById("new-book-items-show-pages");
+// const showNewBookCard = document.querySelector(".new-book-items-card");
+// const showNewBookTitle = document.getElementById("new-book-items-show-title");
+// const showNewBookAuthor = document.getElementById("new-book-items-show-author");
+// const showNewBookPages = document.getElementById("new-book-items-show-pages");
 const showNewBookStatus = document.getElementById("new-book-items-show-status");
-const showNewBookRemove = document.getElementById("new-book-items-show-remove");
+// const showNewBookRemove = document.getElementById("new-book-items-show-remove");
 
-let titleDisplay;
-let authorDisplay;
-let pagesDisplay;
-let optionDisplay;
+// let titleDisplay;
+// let authorDisplay;
+// let pagesDisplay;
+// let optionDisplay;
 
-// titleInput.addEventListener("keydown", (event) => {
-// titleDisplay = event.key;
-// showNewBook.innerText = titleInput.value;
-// title = event.key;
-// console.log(titleDisplay);
-// event.preventDefault();
-// });
-// console.log(title.id);
+// console.log(myLibrary[myLibrary.length-1]);
 
-// authorInput.addEventListener("keydown", (event) => {
-// authorDisplay = event.key;
-// console.log(authorDisplay);
-// });
-
-// numberOfPagesInput.addEventListener("keydown", (event) => {
-// pagesDisplay = event.key;
-// console.log(pagesDisplay);
-// addBookToLibrary();
-// });
-
-// readingOption.addEventListener("change", (event) => {
-//   optionDisplay = readingOption.options[readingOption.selectedIndex].text;
-// console.log(optionDisplay);
-// });
-
-// addLibButton.addEventListener("click", (event) => {
-// showNewBook.textContent = titleInput.value;//this doesn't work
-// showNewBook.textContent = authorInput.value; // only this works
-// });
-
-dialogForm.addEventListener("submit", function (e) {
-  e.preventDefault();
-  let formdata = new FormData(this); // e.target
-
-  let titleInputGet = formdata.get("title");
-  let authorInputGet = formdata.get("author");
-  let pagesInputGet = formdata.get("number-page");
-  let readingOptionGet = formdata.get("book");
-
-  function BookDialog(title, author, pages, readingStatus) {
-    this.title = title;
-    this.author = author;
-    this.pages = pages;
-    this.readingStatus = readingStatus;
-  }
-  const newBookDialog = new BookDialog(
-    titleInputGet,
-    authorInputGet,
-    pagesInputGet,
-    readingOptionGet
-  );
-  // console.log(newBookDialog);
-
-  myLibrary.push(newBookDialog);
-
-  showNewBookTitle.textContent = `" ${newBookDialog.title} "`;
-  showNewBookAuthor.textContent = `by ${newBookDialog.author}`;
-  showNewBookPages.textContent = `${newBookDialog.pages} pages`;
-  showNewBookStatus.textContent = `${newBookDialog.readingStatus}`;
-
-  // console.log(myLibrary[myLibrary.length-1]);
-  addBookToLibrary();
-  addMultipleBookToLibrary();
-});
-
-function addBookToLibrary() {
-  // console.log(title.value);
-  // console.log(author.value);
-  // console.log(readingOption.options[readingOption.selectedIndex].text);
-  // console.log(pagesDisplay); // only last digit show
-  // myLibrary.map((x) => console.log(x.title));
-  // myLibrary.map((x) => console.log(x.author));
-  // myLibrary.map((x) => console.log(x.pages));
-  // myLibrary.map((x) => console.log(x.readingStatus));
-  // myLibrary.map((x) => console.log(x));
+function addBookToLibrary(title, author, pages, readingStatus) {
+  const newBook = new Book(title, author, pages, readingStatus);
+  myLibrary.push(newBook);
 }
 
-function addMultipleBookToLibrary(myLibrary) {
-  return (
-    '<div class="new-book-items-card">' +
-    myLibrary
-      .map(
-        (myLibraryCard) =>
-          `
-        <div id="new-book-items-show-title">${myLibraryCard.title}</div>
-          <div id="new-book-items-show-author">${myLibraryCard.author}</div>
-          <div id="new-book-items-show-pages">${myLibraryCard.pages}</div>
-          <div id="new-book-items-show-status">${myLibraryCard.readingStatus}</div>
-          <div id="new-book-items-show-remove"></div>`
-      )
-      .join("") +
-    "</div>"
-  );
+function displayBooks() {
+  bookContainer.innerHTML = "";
+
+  myLibrary.forEach((book, index) => {
+    const card = document.createElement("div");
+    card.classList.add("card");
+    card.setAttribute("data-book-num", index);
+
+    card.innerHTML = `
+      <div id="new-book-items-show-title">${book.title}</div>
+      <div id="new-book-items-show-author">${book.author}</div>
+      <div id="new-book-items-show-pages">${book.pages} pages</div>
+      <div id="new-book-items-show-status">${book.readingStatus}</div>
+          <div class="card-btn">
+            <button type="button" id="new-book-items-show-status"></button>
+            <button type="button" id="new-book-items-show-remove" class= "new-book-items-show-remove">Remove</button>
+          </div>
+           
+            `;
+    bookContainer.appendChild(card);
+  });
 }
 
 addLibButton.addEventListener("click", () => {
   dialog.close();
+  // dialogForm.reset();
 });
-bookContainer.innerHTML = addBookToLibrary(myLibrary);
+
+dialogForm.addEventListener("submit", (e) => {
+  e.preventDefault();
+
+  let title = titleInput.value;
+  let author = authorInput.value;
+  let pages = numberOfPagesInput.value;
+  let readingStatus = readingOption.value;
+
+  addBookToLibrary(title, author, pages, readingStatus);
+  displayBooks();
+
+  dialogForm.reset();
+  dialog.close();
+
+  // displayBooks();
+});
+
+myLibrary.push(
+  new Book("Crime and Punishment", "Fyodor Dostoevsky", 720, "Read")
+);
+
+displayBooks();
+
+function removeBookListener(button) {
+  button.addEventListener("click", (event) => {
+    const selectedCard = event.target.closest(".card");
+
+    let bookNumber = selectedCard.getAttribute("data-book-num");
+
+    myLibrary.splice(`${bookNumber}`, 1);
+    selectedCard.remove();
+    displayBooks();
+  });
+}
+
+const showNewBookRemove = document.querySelectorAll(
+  ".new-book-items-show-remove"
+);
+
+showNewBookRemove.forEach((button) => removeBookListener(button));
+
+// window.onload = function () {
+//   displayBooks();
+// };
